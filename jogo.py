775d134db2ce8eed7e1afa22acef78a500e1fe79
omarquezini <rafael.marquezini.silva@escola.pr.gov.br>
2025-07-31 12:17:48 +0000

    criando interface e fasendo coisas randon.

---
+++ jogo.py
@@ -0,0 +1,27 @@
+import random
+
+print("#############___MMMMMM___###############")
+print("$$        jogo de adivinhação         $$")
+print("$            O_MARQUEZINI              $")
+print("#############___MMMMMM___###############")
+print("                                        ")
+print("    @@@@             ______________[]___")
+print("  @@@@@@@@@@         [                 ]")
+print("      @@@@@@@@@      [                 ]")
+print("_                    [                 ]")
+print("  \                  [                 ]")
+print("   \                 [                 ]")
+print("    \____       _____[                 ]")
+print("         \     /     [                 ]")
+print("          \___/_____ [                 ]")
+print("                    \[                 ]")
+print("                     [                 ]")
+
+
+
+numeroSecreto = random.randrange(0, 100)
+
+totalTentativas = 0
+pontos = 999
+
+
